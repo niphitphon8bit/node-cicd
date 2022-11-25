0bb3e8e672b6867a40b3135494bc59ac68e43f9c
niphitphon8bit <niphitphon_nt@hotmail.com>
2022-11-25 10:22:49 +0700

    add new test 2

--- src/app.js
+++ src/app.js
@@ -4,4 +4,8 @@ const app = express();
 app.get("/test", (_req, res) => {
     res.status(200).send("Hello world")
 })
+
+app.get("/test2", (_req, res) => {
+    res.status(200).send("Hello Update from github")
+})
 module.exports = app;
