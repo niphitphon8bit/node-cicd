87a638c76bffedd6f2ba08d1446e57c9c19a6aab
niphitphon8bit <niphitphon_nt@hotmail.com>
2022-11-25 10:34:24 +0700

    add testing method

--- src/app.test.js
+++ src/app.test.js
@@ -9,3 +9,19 @@ describe("/test endpoint", () => {
         expect(response.text).toBe("Hello world");
     })
 })
+
+describe("/test2 endpoint", () => {
+    it("should return a response", async () => {
+        const response = await request.get("/test2")
+        expect(response.status).toBe(200)
+        expect(response.text).toBe("Hello Update from github");
+    })
+})
+
+describe("/test3 endpoint", () => {
+    it("should return a response", async () => {
+        const response = await request.get("/test3")
+        expect(response.status).toBe(200)
+        expect(response.text).toBe("Hello Update from github3");
+    })
+})
